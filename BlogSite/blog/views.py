8fd77938f0ca02ecb5548bd34a6dcf948e7d099c
Divyansh Arya <divyansh15047arya@gmail.com>
2020-05-15 23:19:25 +0530

    created PostDetailView

--- BlogSite/blog/views.py
+++ BlogSite/blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Post
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 
 def home(request):
@@ -14,6 +14,12 @@ class PostListView(ListView):
     model = Post
     template_name = 'blog/home.html'
     context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = Post
+
 
 
 def about(request):
